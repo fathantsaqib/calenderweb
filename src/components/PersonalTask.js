a4a29cb61d7b309e334a45a7038c3fb296fe995c
Henokh Abhinaya Tjahjadi <henokhtjahjadi@gmail.com>
2024-07-03 12:27:20 +0800

    fix personal task

--- src/components/PersonalTask.js
+++ src/components/PersonalTask.js
@@ -1,38 +1,3 @@
-<<<<<<< Updated upstream
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import './task.module.css'; // Import CSS
-
-const PersonalTaskList = () => {
-  const [tasks, setTasks] = useState([]);
-
-  useEffect(() => {
-    axios.get('/api/personaltasks')
-      .then(response => {
-        setTasks(response.data);
-      })
-      .catch(error => {
-        console.error('There was an error fetching the tasks!', error);
-      });
-  }, []);
-
-  return (
-    <div className="container">
-      <h1>Personal Tasks</h1>
-      <ul>
-        {tasks.map(task => (
-          <li key={task.task_id}>
-            <h2>{task.task_description}</h2>
-            <p><span>Due Date:</span> {new Date(task.due_date).toLocaleDateString()}</p>
-            <p><span>Status:</span> {task.status}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-=======
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom'; 
 import { getTasks, createTask, updateTask, deleteTask } from '../services/Api'; // Import getTasks dan createTask dari Api.js
@@ -201,5 +166,4 @@ const PersonalTaskList = () => {
   );
 };
 
->>>>>>> Stashed changes
 export default PersonalTaskList;
